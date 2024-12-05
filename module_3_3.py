35693fa4e9f30fea32333538e67c2d959352ef9c
alphadekw <163714728+alphadekw@users.noreply.github.com>
2024-12-05 23:27:16 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,14 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = [33, True, 'hello']
+values_dict = {"a": 1, "b": 'строка', 'c': True}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [10, False]
+print_params(*values_list_2, 42)
